d95ff8daec8e8e434a6593d61b2c221942608631
Yui <julia.ap@babushka.solutions>
2020-01-19 20:49:58 +0200

    final fix

--- src/app/pages/authentication-page/authentication-page.component.ts
+++ src/app/pages/authentication-page/authentication-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {UserService} from '../../services/user.service';
 import {Router} from '@angular/router';
--- src/app/pages/one-product-page/one-product-page.component.ts
+++ src/app/pages/one-product-page/one-product-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ProductsService} from '../../services/products.service';
 import {FormBuilder, FormGroup} from '@angular/forms';
